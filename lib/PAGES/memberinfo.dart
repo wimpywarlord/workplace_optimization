28c1cda76890c9c334850b4892f3e7b01e84212f
wimpywarlord <wimpywarlord@gmail.com>
2019-05-24 17:50:23 +0530

    deleted the practice code and started implementing real app with partially complete first page'

--- lib/PAGES/memberinfo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Memberinfo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(child: Text('THIS IS MEMBER\'s INFO'),);
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/rendering.dart';
 
-import './PAGES/deck.dart';
-
 void main() {
-  //debugPaintSizeEnabled = true;
-  runApp(Home());
+  //debugPaintSizeEnabled = true ;
+  return runApp(Start());
 }
 
-class Home extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _Home();
-  }
-}
-
-class _Home extends State<Home> {
+class Start extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromRGBO(190, 190, 190, 2),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        body: Stack(
           children: <Widget>[
             Container(
-              child: Image.asset('assets/logo.png'),
-              height: 200.0,
-              width: 500.0,
-              alignment: Alignment(0.0, 0.0),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/page1.jpg'),
+                    fit: BoxFit.fitHeight),
+              ),
             ),
             Container(
-              margin: EdgeInsets.all(7.0),
-              child: Text(
-                'WELCOME TO',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20.0,
+              color: Color.fromRGBO(255, 255, 255, 0.19),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Center(
+                child: Text(
+                  'LETS BE PRODUCTIVE TODAY',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      fontFamily: 'Impact',
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(255, 255, 255, 1)),
                 ),
               ),
             ),
-            Text(
-              'INIT',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 40.0,
+            Container(
+              child: Row(
+                children: <Widget>[],
               ),
-            ),
-           Button() 
+            )
           ],
         ),
       ),
     );
-  }
-}
-
-class Button extends StatelessWidget 
-{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RaisedButton(
-              elevation: 20.0,
-              onPressed: () {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return Deck();
-                }));
-              },
-              child: Text('LET\'S GO'),
-            );
   }
 }
