909f15278011db4a97094f4dee1bd93def67593b
wimpywarlord <wimpywarlord@gmail.com>
2019-06-07 10:48:45 +0530

    saving data of form field pushing to server left

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -12,6 +14,8 @@ class SignUpPageState extends State<SignUpPage> {
   String password;
   String skill;
   String mobileno;
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -31,119 +35,127 @@ class SignUpPageState extends State<SignUpPage> {
           Container(
             color: Color.fromRGBO(255, 255, 255, 0.49),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                child: TextFormField(
-                  onSaved: (String value) {
-                    username = value;
-                  },
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Username',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          20.0,
+          Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      username = value;
+                    },
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Username',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            20.0,
+                          ),
                         ),
                       ),
                     ),
+                    maxLength: 20,
                   ),
-                  maxLength: 20,
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                child: TextFormField(
-                  obscureText: true,
-                  onSaved: (String value) {
-                    password = value;
-                  },
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          20.0,
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    onSaved: (String value) {
+                      password = value;
+                    },
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            20.0,
+                          ),
                         ),
                       ),
                     ),
+                    maxLength: 20,
                   ),
-                  maxLength: 20,
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                child: TextFormField(
-                  onSaved: (String value) {
-                    skill = value;
-                  },
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Skill eg :- Web Dev , App Dev etc. ',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          20.0,
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      skill = value;
+                    },
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Skill eg :- Web Dev , App Dev etc. ',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            20.0,
+                          ),
                         ),
                       ),
                     ),
+                    maxLength: 20,
                   ),
-                  maxLength: 20,
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                child: TextFormField(
-                  onSaved: (String value) {
-                    mobileno = value;
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: 'Mobile No.',
-                    contentPadding: EdgeInsets.all(20.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          20.0,
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      mobileno = value;
+                    },
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Mobile No.',
+                      contentPadding: EdgeInsets.all(20.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            20.0,
+                          ),
                         ),
                       ),
                     ),
+                    maxLength: 20,
                   ),
-                  maxLength: 20,
                 ),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Map<String, String> userdata = {
-                    'username': username,
-                    'password': password,
-                    'skill': skill,
-                    'mobileno': mobileno
-                  };
-                  print(userdata);
-                },
-                color: Colors.white,
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+                RaisedButton(
+                  onPressed: () {
+                    _formkey.currentState.save();
+                    Map<String, String> userdata = {
+                      'username': username,
+                      'password': password,
+                      'skill': skill,
+                      'mobileno': mobileno
+                    };
+                    http.post(
+                      'https://workplace-e3f7b.firebaseio.com/userdata.json',
+                      body: json.encode(userdata),
+                    );
+                    print(userdata);
+                  },
+                  color: Colors.white,
+                  elevation: 10.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text('SIGN IN'),
                 ),
-                child: Text('SIGN IN'),
-              ),
-            ],
-          )
+              ],
+            ),
+          ),
         ],
       )),
     );
