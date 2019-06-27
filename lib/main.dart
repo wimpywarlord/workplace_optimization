6f16c579f0da49a8df62401f4a9d44c2f0d68342
wimpywarlord <wimpywarlord@gmail.com>
2019-06-27 22:51:49 +0530

    added new future updates and corrrect fully fucntional apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,153 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter/rendering.dart';
+            import 'package:flutter/material.dart';
+            import 'package:flutter/widgets.dart';
+            import 'package:flutter/rendering.dart';
 
-import './page2.dart';
-import './page3.dart';
-import './page4.dart';
+            import './page2.dart';
+            import './page3.dart';
+            import './page4.dart';
 
-void main() {
-  debugPaintSizeEnabled = true ;
-  return runApp(Start());
-}
+            void main() {
+              //debugPaintSizeEnabled = true ;
+              return runApp(Start());
+            }
 
-class Start extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: 'InIt',
-      home: Builder(builder: (context) {
-        return GestureDetector(
-          onTap: () {
-            return Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return Page2();
-                },
-              ),
-            );
-          },
-          child: Scaffold(
-            body: Stack(
-              children: <Widget>[
+            class Start extends StatelessWidget {
 
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('images/page1.jpg'),
-                        fit: BoxFit.fitHeight),
-                  ),
-                ),
-                Container(
-                  color: Color.fromRGBO(255, 255, 255, 0.19),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Center(
-                    child: Text(
-                      'LETS BE PRODUCTIVE TODAY',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 50.0,
-                          fontFamily: 'bold',
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(255, 255, 255, 1)),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      RawMaterialButton(
-                        onPressed: () {},
-                        constraints:
-                            BoxConstraints.tightFor(height: 10.0, width: 10.0),
-                        shape: CircleBorder(),
-                        fillColor: Colors.white,
+              @override
+              Widget build(BuildContext context) {
+                // TODO: implement build
+                return MaterialApp(
+                  title: 'InIt',
+                  home: Builder(builder: (context) {
+                    return GestureDetector(
+                      onTap: () {
+                        return Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return Page2();
+                            },
+                          ),
+                        );
+                      },
+                      child: Scaffold(
+                        body: Stack(
+                          children: <Widget>[
+
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/page1.jpg'),
+                                    fit: BoxFit.fitHeight),
+                              ),
+                            ),
+                            Container(
+                              color: Color.fromRGBO(255, 255, 255, 0.19),
+                            ),
+                            Container(
+                              alignment: Alignment.center,
+                              child: Center(
+                                child: Text(
+                                  'LETS BE PRODUCTIVE TODAY',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 50.0,
+                                      fontFamily: 'bold',
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(255, 255, 255, 1)),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 10.0),
+                              alignment: Alignment.bottomCenter,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  RawMaterialButton(
+                                    onPressed: () {},
+                                    constraints:
+                                        BoxConstraints.tightFor(height: 10.0, width: 10.0),
+                                    shape: CircleBorder(),
+                                    fillColor: Colors.white,
+                                  ),
+                                  Page2call(),
+                                  Page3call(),
+                                  Page4call(),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                      Page2call(),
-                      Page3call(),
-                      Page4call(),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-      }),
-    );
-  }
-}
+                    );
+                  }),
+                );
+              }
+            }
 
-class Page2call extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RawMaterialButton(
-      onPressed: () {
-        return Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return Page2();
-            },
-          ),
-        );
-      },
-      constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
-      shape: CircleBorder(),
-      fillColor: Colors.white,
-    );
-  }
-}
+            class Page2call extends StatelessWidget {
+              @override
+              Widget build(BuildContext context) {
+                // TODO: implement build
+                return RawMaterialButton(
+                  onPressed: () {
+                    return Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return Page2();
+                        },
+                      ),
+                    );
+                  },
+                  constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
+                  shape: CircleBorder(),
+                  fillColor: Colors.white,
+                );
+              }
+            }
 
-class Page3call extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RawMaterialButton(
-      onPressed: () {
-        return Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return Page3();
-            },
-          ),
-        );
-      },
-      constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
-      shape: CircleBorder(),
-      fillColor: Colors.white,
-    );
-  }
-}
+            class Page3call extends StatelessWidget {
+              @override
+              Widget build(BuildContext context) {
+                // TODO: implement build
+                return RawMaterialButton(
+                  onPressed: () {
+                    return Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return Page3();
+                        },
+                      ),
+                    );
+                  },
+                  constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
+                  shape: CircleBorder(),
+                  fillColor: Colors.white,
+                );
+              }
+            }
 
-class Page4call extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RawMaterialButton(
-      onPressed: () {
-        return Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return Page4();
-            },
-          ),
-        );
-      },
-      constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
-      shape: CircleBorder(),
-      fillColor: Colors.white,
-    );
-  }
-}
+            class Page4call extends StatelessWidget {
+              @override
+              Widget build(BuildContext context) {
+                // TODO: implement build
+                return RawMaterialButton(
+                  onPressed: () {
+                    return Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return Page4();
+                        },
+                      ),
+                    );
+                  },
+                  constraints: BoxConstraints.tightFor(height: 10.0, width: 10.0),
+                  shape: CircleBorder(),
+                  fillColor: Colors.white,
+                );
+              }
+            }
